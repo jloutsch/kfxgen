eef606ad8558ffbde5318696b0ee22fecff201f4
Justin L <justin.loutsch@gmail.com>
2026-06-28 15:49:36 -0400

    feat(emphasis): normalize_runs span builder for inline styling (#9)

---
+++ tests/unit/test_inline_style.py
@@ -0,0 +1,42 @@
+import pytest
+from kfxgen import inline_style as ist
+from kfxgen.inline_style import FLAG_ITALIC as I, FLAG_BOLD as B
+
+
+@pytest.mark.unit
+def test_plain_text_no_spans():
+    text, spans = ist.normalize_runs([("hello world", frozenset())])
+    assert text == "hello world"
+    assert spans == []
+
+
+@pytest.mark.unit
+def test_single_italic_run():
+    text, spans = ist.normalize_runs(
+        [("a ", frozenset()), ("big", frozenset({I})), (" cat", frozenset())]
+    )
+    assert text == "a big cat"
+    assert spans == [(2, 3, frozenset({I}))]
+
+
+@pytest.mark.unit
+def test_whitespace_collapsed_and_stripped():
+    text, spans = ist.normalize_runs(
+        [("  a\n\n", frozenset()), ("  b  ", frozenset({B}))]
+    )
+    assert text == "a b"
+    assert spans == [(2, 1, frozenset({B}))]
+
+
+@pytest.mark.unit
+def test_bold_italic_combined_flags():
+    text, spans = ist.normalize_runs([("x", frozenset({I, B}))])
+    assert text == "x"
+    assert spans == [(0, 1, frozenset({I, B}))]
+
+
+@pytest.mark.unit
+def test_adjacent_same_flags_merge():
+    text, spans = ist.normalize_runs([("ab", frozenset({I})), ("cd", frozenset({I}))])
+    assert text == "abcd"
+    assert spans == [(0, 4, frozenset({I}))]
